403af11a72b4fcf921ab91978a807d52b2391001
Pablo Lario Gómez <62502964+Thelario@users.noreply.github.com>
2022-04-02 13:03:39 +0200

    Vertex Buffers in OpenGL

--- Learning OpenGL/Learning OpenGL/src/Application.cpp
+++ Learning OpenGL/Learning OpenGL/src/Application.cpp
@@ -4,6 +4,9 @@
 
 int main(void)
 {
+
+#pragma region Initializing GLFW & GLEW
+
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -21,8 +24,7 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    //glutInit(&argc, argv);
-    //glutCreateWindow("GLEW Test");
+    /* Initializing GLEW */
     GLenum err = glewInit();
     if (GLEW_OK != err)
     {
@@ -34,11 +36,59 @@ int main(void)
     fprintf(stdout, "Status: Using OpenGL %s\n", glGetString(GL_VERSION));
     fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 
+#pragma endregion
+
+#pragma region Vertex Buffer
+
+    /* CREATING A VERTEX BUFFER THAT CONTAINS THE DATA TO DRAW A TRIANGLE */
+
+    /* These are going to be the vertices of the triangle. */
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    /* In this case, we are returning 1 buffer object into buffer. */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer); 
+    
+    /* Once we have generated a buffer, we need to select it, and that is done with glBindBuffer. */
+    /* In this case, we are binding the buffer into GL_ARRAY_BUFFER, which simply means that the */
+    /* purpose of the buffer will be of being an array of Vertex Attributes. */
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); 
+
+    /* We now need to give data to the buffer. This can be done either by directly giving data to the buffer */
+    /* or we can give it nothing and later pass the data. We are going to provide data straighaway. */
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), &positions, GL_STATIC_DRAW);
+
+#pragma endregion
+
+#pragma region Main Loop
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+#pragma region Drawing 
+
+        /* We can draw in two different ways: */
+        /* 1) glDrawArrays: we use this if we do not have an IndexBuffer. */
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* 2) glDrawElements: we use this if we have an IndexBuffer. */
+        /* glDrawElements(); */
+
+        /* How does OpenGL know that it has to draw the triangle specified before? */
+        /* OpenGL will draw whatever is currently bound. In our case, we called to */
+        /* glBindBuffer before, and haven't bound anything else, so it will draw the */
+        /* triangle that has been passed to the buffer. */
+        /* If before having called to glDrawArrays we had bound a different buffer, then */
+        /* OpenGL would draw what is contained in that buffer, and not the triangle we want to draw.*/
+
+#pragma endregion
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -49,4 +99,6 @@ int main(void)
 
     glfwTerminate();
     return 0;
+
+#pragma endregion
 }
